90817669c0926b341ee511eaa976d63a445c2abb
christianjurt <Longbeach@2>
2017-10-08 17:57:19 -0400

    files are set up!

---
+++ js/Main.js
@@ -0,0 +1,10 @@
+/*****************************************************************
+File: Main.js
+Author: Christian Jurt & Mitch Toutain
+
+Description: This is App allows users to catelogues and inventories beverages at special events.
+
+Version: 0.0.1
+Updated: Oct 8, 2017
+
+*****************************************************************/
